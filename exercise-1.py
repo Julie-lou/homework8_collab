601a5a2924becb4941fab50f9e3b571e2c7be183
pichmarina <137904937+pichmarina@users.noreply.github.com>
2023-07-31 23:11:30 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,12 @@
+numbers=[1,2,3,4]
 def replace_last(numbers):
-    ...
+    if len(numbers)<=1:
+        return numbers
+    else:
+        temp=numbers[0]
+        numbers[0]=numbers[-1]
+        numbers[-1]=temp
+        return numbers
+        
+    
+    
