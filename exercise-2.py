f35917c427dced91f0e5dd0ef231f27ab845f8a5
Julie-lou <137873538+Julie-lou@users.noreply.github.com>
2023-07-31 22:08:40 +0700

    Update exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,14 @@
-def index_power(numnbers, n):
-    ...
+
+numbers = [1,2,3,4]
+def index_power(numbers, n):
+    for x in numbers:
+        if n in range(len(numbers)):
+            y = numbers[n]*numbers[n]
+            return y
+        elif n not in range(len(numbers)):
+            return -1
+
+print(index_power(numbers,2))
+
+
+
