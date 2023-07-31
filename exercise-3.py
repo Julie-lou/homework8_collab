e8cc8d328f311f517c97e2beae651a9d5034bbb4
pichmarina <137904937+pichmarina@users.noreply.github.com>
2023-08-01 00:14:23 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,12 @@
 def remove_all_after(numbers, n):
-    ...
+      list=[]
+    if n in numbers:
+        for number in numbers:
+            if number!=n:
+                list.append(number)
+            elif number==n:
+                list.append(number)
+                break
+        return list
+    elif n not in numbers:
+        return numbers  
