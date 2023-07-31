c2cd32242cb1369545ff5c5f1767df8d817aa39b
pichmarina <137904937+pichmarina@users.noreply.github.com>
2023-07-31 23:18:25 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,11 +1,12 @@
 def replace_last(numbers):
-    if len(numbers)<=1:
-        return numbers
-    else:
-        temp=numbers[0]
-        numbers[0]=numbers[-1]
-        numbers[-1]=temp
-        return numbers
+    for number in numbers:
+        if len(numbers)<=1:
+            return numbers
+        else:
+            temp=numbers[0]
+            numbers[0]=numbers[-1]
+            numbers[-1]=temp
+            return numbers
         
     
     
