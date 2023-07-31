1f10c31bbd77f9162cdf66b92e5a1e490a5cfc78
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2023-07-31 23:46:00 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,13 @@
 def remove_all_after(numbers, n):
+    new = []
+    if n in numbers:
+        for x in numbers:
+            if x != n:
+                new.append(x)
+            elif x == n:
+                new.append(x)
+                break
+        return new
+    elif n not in numbers:
+        return numbers
     ...
