4ffa06bdd1b6056a6c575c03b32d0bb9b22771f1
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2023-07-31 23:17:09 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,8 +1,2 @@
 def replace_last(numbers):
-    if len(numebers) <= 1:
-        return numbers
-    else:
-        numbers[0] = number[-1]
-        numbers.remove(number[-1])
-        return numbers
     ...
