441fc4b6567b98163c6866d4cc418382f258e25c
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2023-08-01 00:23:21 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,10 +1,10 @@
 def chunking_by(numbers, chunk):
-    
+    if len(list) == 0:
+        return list
     for i in range(chunk):
         new = []
         new.append(numbers[0:chunk])
         new.append(numbers[chunk:chunk*2])
         new.append(numbers[chunk*2:chunk*3])
-        
     return new
     ...
