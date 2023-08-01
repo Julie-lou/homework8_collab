e0c712d0dde871bad5fac2f4469039e3f31f8956
pichmarina <137904937+pichmarina@users.noreply.github.com>
2023-08-01 13:54:53 +0700

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,5 @@
 def reverse_ascending(numbers):
-    ...
+    for i in range(1,len(items)): 
+        if items[i] <= items[i-1]:
+            return items[:i][::-1]+reverse_ascending(items[i:])
+    return items[::-1]
