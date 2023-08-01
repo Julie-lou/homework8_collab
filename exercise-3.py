04e52a49b00659429f7cd19ccdd00e78ec69a450
Julie-lou <137873538+Julie-lou@users.noreply.github.com>
2023-08-01 19:09:07 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,1 +1,16 @@
+def remove_all_after(numbers, n):
+    for x in numbers:
+        new_list = []
+        if n in range(len(numbers)):
+            y = numbers[0:n:]
+            if numbers.count(n) > 1:
+                first_n = numbers.index(n)
+                second_n = numbers.index(n, + first_n +1)
+                y = numbers[0:second_n:]
+                
+            new_list.append(y)
+        return new_list
+print(remove_all_after([1, 2, 3, 4, 5], 3))
+
+
 
