0ebdf99f50b6ca307bc1d295c567b8b305ad49a8
pichmarina <137904937+pichmarina@users.noreply.github.com>
2023-08-01 13:30:05 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,11 @@
 def chunking_by(numbers, chunck):
-    ...
+    length=(len(numbers)//chunk)
+    length_1=length+1
+    if len(numbers)==0:
+        return numbers
+    new_list=[]
+    n=1
+    for i in range(length_1):        
+        new_list.append(numbers[chunk*(n-1):chunk*n])
+        n+=1    
+    return new_list
