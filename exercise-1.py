b63b690296e5b866b0d8a7c9f41164416e2c1c40
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2023-08-01 19:32:13 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,1 +1,12 @@
 
+def remove_last(numbers):
+
+    if len(numbers) <= 1:
+        return numbers
+    else:
+        for x in numbers:
+            a = numbers[0]
+            numbers[0] = numbers[-1]
+            numbers.insert(1, a)
+
+            return numbers[0:-1]
