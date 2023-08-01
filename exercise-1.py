cda07ec267a71175ce7ce1292f5543cf58758fdc
Julie-lou <137873538+Julie-lou@users.noreply.github.com>
2023-08-01 10:19:31 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,15 @@
+
+numbers = [1, 2, 3, 4]
 def replace_last(numbers):
-    ...
+    for x in numbers:
+        numbers.append(numbers.pop(0))
+        numbers.append(numbers.pop(0))
+        numbers.append(numbers.pop(0))
+
+        return numbers
+    
+print (replace_last(numbers))
+
+
+
+
